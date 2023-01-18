e883f0013eda5f280e15ce5cb09dbe8d91df17d8
IvanGLS <gladiolus0707@gmail.com>
2023-01-18 16:57:58 +0200

    Update comments form

--- portal/forms.py
+++ portal/forms.py
@@ -27,15 +27,16 @@ class NewsSearchForm(forms.Form):
 class CommentsForm(forms.ModelForm):
     class Meta:
         model = Comments
-        content = forms.CharField(widget=forms.Textarea(attrs={"rows": 7}), label=None)
         fields = ("content",)
+        labels = {
+            "content": ""
+        }
 
-
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     for field in self.fields:
-    #         self.fields[field].widget.attrs["class"] = "form-control"
-    #     self.fields["content"].widget = Textarea(attrs={"rows": 7})
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs["class"] = "form-control"
+        self.fields["content"].widget = Textarea(attrs={"rows": 7})
 
 
 class NewsUpdateForm(forms.ModelForm):
